9c4497c7f8ab93d0544493c82e3744d7d462bbf0
Evan Bay <neopythe@protonmail.ch>
2022-07-23 01:27:23 -0600

    add icons to Todo component

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,14 +1,25 @@
 import ListItem from '@mui/material/ListItem'
 import ListItemText from '@mui/material/ListItemText'
-import { Checkbox, ListItemSecondaryAction } from '@mui/material'
-// import IconButton from '@mui/material'
-// import ListItemSecondaryAction from '@mui/material'
+import ListItemSecondaryAction from '@mui/material/ListItemSecondaryAction'
+import Checkbox from '@mui/material/Checkbox'
+import IconButton from '@mui/material/IconButton'
+import { Delete, Edit } from '@mui/icons-material'
 
 export default function Todo({ task, completed }) {
   return (
     <ListItem>
       <Checkbox checked={completed} tabIndex={-1} />
-      <ListItemText>{task}</ListItemText>
+      <ListItemText style={{ textDecoration: completed && 'line-through' }}>
+        {task}
+      </ListItemText>
+      <ListItemSecondaryAction>
+        <IconButton aria-label="Edit">
+          <Edit />
+        </IconButton>
+        <IconButton aria-label="Delete">
+          <Delete />
+        </IconButton>
+      </ListItemSecondaryAction>
     </ListItem>
   )
 }
